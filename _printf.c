20db6882f49d7cd33db3269a0e658e78996c59ee
root <juanlds621@gmail.com>
2022-07-20 14:58:59 -0700

    first part to proyect

---
+++ _printf.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ */
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int lenght = 0;
+	print_a a[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"d", print_d},
+		{"i", print_i},
+		{NULL, NULL}
+	};
+	va_start(arg, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+	lenght = aux_func(format, arg, a);
+	va_end(arg);
+	return (lenght);
+}
---
+++ aux_func.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ */
+int aux_func(const char *format, va_list arg, print_a *a)
+{
+	int v = 0, m, size = 0, test = 0;
+
+	while (format && format[v])
+	{
+		if (format[v] == '%')
+		{
+			m = 0;
+			while (a[m].j != NULL)
+			{
+				if (format[v + 1] == '%')
+				{
+					write(1, &format[v], 1);
+					size++;
+					v++;
+					test = 1;
+					break;
+				}
+				else if (a[m].j[0] == format[v + 1])
+				{
+					size += a[m].k(arg);
+					v++;
+					test = 1;
+					break;
+				}
+				m++;
+			}
+			if (test == 0)
+			{
+				write(1, &format[v], 1);
+				size++;
+			}
+		}
+		else
+		{
+			write(1, &format[v], 1);
+			size++;
+		}
+		v++;
+	}
+	return (size);
+}
