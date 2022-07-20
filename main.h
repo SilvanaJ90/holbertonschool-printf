9638de76098453124cd0cd166a6b1d9924f0d085
root <juanlds621@gmail.com>
2022-07-20 09:01:35 -0700

    add header file

--- main.h
+++ main.h
@@ -1,8 +1,24 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 #include <stdio.h>
+/**
+ * struct print - function of variadic list
+ * @j: to print content
+ * @k: print what you get
+ */
+typedef struct print
+{
+	char *j;
+	int (*k)(va_list);
+} print_t;
 
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_d(va_list d);
+int print_i(va_list i);
 
 #endif /* MAIN_H */
