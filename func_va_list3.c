57b520ec89756c23bc647919492fbe5d25fb1788
root <juanlds621@gmail.com>
2022-07-22 09:25:21 -0700

    add hex cod

--- func_va_list3.c
+++ func_va_list3.c
@@ -1,6 +1,23 @@
 #include "main.h"
 /**
  */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
 int print_S(va_list arg)
 {
 	unsigned int i;
