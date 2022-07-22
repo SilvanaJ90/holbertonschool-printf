83dcdbbe15745b0c7b0248ebf836c05811673cd1
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-07-21 22:39:46 -0500

    update-main.h

--- main.h
+++ main.h
@@ -22,5 +22,9 @@ int print_s(va_list arg);
 int print_pct(va_list arg);
 int print_d(va_list arg);
 int print_i(va_list arg);
+int print_u(va_list arg);
+int print_o(va_list arg);
+int print_x(va_list arg);
+int print_X(va_list arg);
 
 #endif /* MAIN_H */
